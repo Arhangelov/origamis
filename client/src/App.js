7bad01d4485af7ae49483562bb48520e3f5381a9
Petyo Arhangelov <73829572+Arhangelov@users.noreply.github.com>
2021-03-20 12:27:08 +0200

    Show posts with filthering

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,56 @@
-import Component from 'react';
+import { Component } from 'react';
+
+import * as postService from './services/postService';
+
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Main from './components/Main/Main';
 
 import style from './App.module.css';
 
-// function App() {
-//     return (
-            // <div className={style.app}>
-            //     <Header />
-            //     <div className={style.container}>
-            //         <Menu />
-            //         <Main />
-            //     </div>
-            // </div>
-//      )
-// }
-
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: [],
+            selectedPost: null,
+        }
+
+        this.onMenuItemClick = this.onMenuItemClick.bind(this);
+    }
+
+    componentDidMount() {
+        postService.getAll()
+            .then(posts => {
+                this.setState({posts})
+            });
+    }
+
+    onMenuItemClick(id) {
+        this.setState({selectedPost: id});
+    }
+
+    getPosts () {
+        if(!this.state.selectedPost) {
+            return this.state.posts;
+        } else {
+           return [this.state.posts.find(x => x.id == this.state.selectedPost)];
+
+        }
+    }
+
     render() {
         return (
             <div className={style.app}>
                 <Header />
 
                 <div className={style.container}>
-                    <Menu />
+                    <Menu 
+                       onMenuItemClick={this.onMenuItemClick} />
 
-                    <Main />
+                    <Main posts={this.getPosts()} />
                 </div>
             </div>
         )
@@ -34,3 +58,16 @@ class App extends Component {
 }
 
 export default App;
+
+
+// function App() {
+//     return (
+// <div className={style.app}>
+//     <Header />
+//     <div className={style.container}>
+//         <Menu />
+//         <Main />
+//     </div>
+// </div>
+//      )
+// }
